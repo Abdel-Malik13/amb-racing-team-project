1df87494b3e8e7a69627b42d75b87d32db82745f
Abdel-Malik BOUALI <Ushiden13@gmail.com>
2025-08-21 16:43:19 +0200

    Add files project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo.png";
+import YouTube from "./images/youtube.png";
+import Instagram from "./images/instagram.png";
+import TikTok from "./images/tiktok.png";
+import Facebook from "./images/facebook.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app-container">
+      <div className="logo">
+        <img
+          className="logoImg"
+          src={logo}
+          alt="Logo de l'équipe de karting AMB Racing Team"
+        />
+      </div>
+      <div>
+        <p className="textSubtitle">
+          Notre site arrive bientôt - <br /> restez connectés !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <ul className="socials">
+          <li>
+            <a href="https://www.youtube.com/@AbdelMalikBOUALI">
+              <img src={YouTube} alt="Logo YouTube" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/ambouali13">
+              <img src={Instagram} alt="Logo Instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.tiktok.com/@ambouali13">
+              <img src={TikTok} alt="Logo TikTok" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/AMBOUALI">
+              <img src={Facebook} alt="Logo Facebook" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

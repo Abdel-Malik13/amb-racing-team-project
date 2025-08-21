e74261771507e0b8431372a17c9db75de2d7861f
Abdel-Malik BOUALI <Ushiden13@gmail.com>
2025-08-21 20:19:30 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import TikTok from "./images/tiktok.png";
 import Facebook from "./images/facebook.png";
 import "./App.css";
 
-import { Analytics } from "@vercel/analytics/react";
-
 function App() {
   return (
     <div className="app-container">
